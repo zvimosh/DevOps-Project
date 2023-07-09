e2be3c740f551e9f948c2f1fc1849cc8b8757b07
zvimosh <zvimoshko@gmail.com>
2023-07-09 19:37:12 +0300

    fixed destroy in deploy workflow

--- terraform-kubeadm/main.tf
+++ terraform-kubeadm/main.tf
@@ -4,8 +4,6 @@ provider "aws" {
 module "cluster" {
   source  = "weibeld/kubeadm/aws"
   version = "~> 0.2"
-  private_key_file = var.private_key_file
-  public_key_file = var.public_key_file
 }
 terraform {
   backend "s3" {
